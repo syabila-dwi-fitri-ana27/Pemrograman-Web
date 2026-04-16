4326be116dde0a82be6096b242e1b06ee3516b2b
syabila-dwi-fitri-ana27 <syabiladwifitriana34@gmail.com>
2026-04-16 16:03:11 +0700

    Pertemuan 3 dan 4

---
+++ pertemuan4.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 4</title>
+</head>
+<body>
+    <h3>Praktikum Pertemuan 4</h3>
+    <table border="1">
+        <tr>
+            <th colspan="5">Data Mahasiswa</th>
+        </tr>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Alamat</th>
+            <th>Umur</th>
+            <th>Foto</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Rizky</td>
+            <td>JL. Merdeka No. 456</td>
+            <td>20</td>
+            <td><img src="https://tse2.mm.bing.net/th/id/OIP.BmsCyGX0G9xpPcnZZejnRAHaHd?pid=Api&P=0&h=180" alt="Foto Rizky" width= "100"></td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Siti</td>
+            <td>Jl. Sudirman No. 456</td>
+            <td>22</td>
+            <td><img src="https://tse4.mm.bing.net/th/id/OIP.-I_CtWH66u88K66jHZXNSQHaF9?pid=Api&P=0&h=180" alt="Foto Siti" width= "100"></td>
+        </tr>
+    </table>
+
+    <table border="1">
+        <tr>
+            <th colspan="5">Daftar Smarthphone</th>
+        </tr>
+        <tr>
+            <th>No</th>
+            <th>Nama Brand</th>
+            <th>Gambar</th>
+            <th>Keterangan</th>
+        </tr>
+        <tr>
+
+        <tr>
+            <td rowspan="3">1</td>
+            <td rowspan="3">Apple</td>
+            <td rowspan="3">
+            <img src="https://tse2.mm.bing.net/th/id/OIP.cv9TzyC_JhXRtMplLUv6pgHaEK?pid=Api&P=0&h=180" alt="foto", width="100"></td>
+            <td>iPhone 16 Pro Max</td>
+        </tr>
+        <tr>
+            <td>Dengan desain titanium yang memukau. Kontrol kamera. Dolby Vision 4K 120 fps.</td>
+        </tr>
+        <tr>
+            <td>Rp 20.000.000</td>
+        </tr>
+        </tr>
+    
+            
+        <tr>
+            <td rowspan="3">2</td>
+            <td rowspan="3">Samsung</td>
+            <td rowspan="3">
+                <img src="https://tse4.mm.bing.net/th/id/OIP.Uup6WrzBfIkQEfpFxl9BSAHaEK?pid=Api&P=0&h=180" alt="foto", width="100"></td>
+            <td>Samsung Galaxy S24 Ultra</td>
+        </tr>
+        <tr>
+            <td>Hp premium dengan layar besar, S Pen, kamera canggih, dan performa sangat mewah</td>
+        </tr>
+        <tr>
+            <td>Rp.18.000.000</td>
+        </tr>
+        </tr>
+        <tr>
+            <td rowspan="3">3</td>
+            <td rowspan="3">Xiamoi</td>
+            <td rowspan="3">
+                <img src="https://tse4.mm.bing.net/th/id/OIP.5XHxlCstfKzLPPYUpiIwwgHaHa?pid=Api&P=0&h=180" alt="foto", width="80"></td>
+            <td>Xiaomi 12 8/256</td>
+        <tr>
+            <td>HP flagship dengan performa kencang, desain, elegan, dan harga lebiih terjangkau</td>
+        </tr>
+        <tr>
+            <td>Rp.8.000.000</td>
+        </tr>
+               
+        </tr>
+    </table>
+</body>
+</html>
